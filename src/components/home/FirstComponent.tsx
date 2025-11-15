ed4451fafb3a95b00b974afe2f5250dcbd4c4313
Axel Munoz <naclif30@gmail.com>
2025-11-15 12:23:08 -0300

    feat: tabs new artist

--- src/components/home/FirstComponent.tsx
+++ src/components/home/FirstComponent.tsx
@@ -1,15 +1,12 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Badge } from "@/components/ui/badge"
+import { Card, CardHeader } from "@/components/ui/card"
+import { artistas } from "@/data/artistas"
+import { Button } from "@/components/ui/button"
 
 
 export const FirstComponent = () => {
-  const artista = {
-    name: 'Patio Solar',
-    genre: 'Indie Rock',
-    location: 'Santiago',
-    description: 'Patio Solar es una banda chilena de indie rock conocida por su sonido fresco y letras introspectivas. Formada en 2015, han lanzado varios álbumes que han sido bien recibidos por la crítica y el público.',
-    imageUrl: 'https://www.musicapopular.cl/wp-content/uploads/2016/09/patiosolar-1.jpg'
-  }
+  const artista = artistas[0]; 
   return (
     <section className="w-full min-h-screen py-16">
       <div className="flex justify-center max-w-7xl mx-auto px-4 items-center">
@@ -19,12 +16,14 @@ export const FirstComponent = () => {
             <TabsTrigger className="rounded-md" value="new-artist">Nuevos artistas</TabsTrigger>
           </TabsList>
 
+          {/* Artista del Día Tab */}
+
           <TabsContent value="art-of-day" className="w-full mt-8">
             <h1 className="font-bold pb-4 text-xl">Artista del día</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
               <div className="overflow-hidden rounded-lg  ">
                 <img src={artista.imageUrl} alt={artista.name}
-                className="w-full h-full rounded-xl hover:scale-120 object-cover transition-transform duration-300" 
+                className="w-full h-full rounded-xl hover:scale-120 object-cover transition-transform duration-300 shadow-md hover:shadow-lg transition" 
                 />
               </div>
 
@@ -40,8 +39,28 @@ export const FirstComponent = () => {
               
             </div>
           </TabsContent>
+
+          {/* Nuevos Artistas Tab */}
           <TabsContent value="new-artist" className="w-full mt-8">
-              <div>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+
+                {artistas.map((art) => (
+                  <Card key={art.id} className="p-0 overflow-hidden rounded-xl shadow-md hover:shadow-lg transition pb-3">
+                    <div className="h-48 w-full overflow-hidden rounded-t-lg">
+                      <img 
+                      src={art.imageUrl} 
+                      alt={art.name} 
+                      className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"/>
+                    </div>
+                    <div>
+                      <CardHeader className="text-center">{art.name}</CardHeader>
+                    </div>
+                    <div className="flex justify-center">
+
+                      <Button variant="outline" className="cursor-pointer" >Explorar</Button>
+                    </div>
+                  </Card>
+                ) )}
                 
               </div>
           </TabsContent>
---
+++ src/data/artistas.ts
@@ -0,0 +1,50 @@
+export const artistas = [
+  {
+    "id": 1,
+    "name": "Ases Falsos",
+    "genre": "Indie Rock / Pop Alternativo",
+    "location": "Santiago",
+    "description": "Ases Falsos es una de las bandas más influyentes del indie chileno, conocida por sus letras directas y su mezcla de rock, pop y tintes latinoamericanos. Liderados por Cristóbal Briceño, han lanzado discos icónicos dentro de la escena local.",
+    "imageUrl": "https://www.valparaisocreativo.cl/wp-content/uploads/2024/01/Ases-Falsos-980x653.jpeg"
+  },
+  {
+    "id": 2,
+    "name": "Los Animales También se Suicidan",
+    "genre": "Indie Pop / Dream Pop",
+    "location": "Santiago",
+    "description": "Los Animales También se Suicidan es un proyecto indie pop de atmósferas melancólicas y melodías suaves. Su sonido se caracteriza por guitarras limpias, voces íntimas y una fuerte carga emocional.",
+    "imageUrl": "https://www.rockaxis.com/img/newsList/5272051.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Amarga Marga",
+    "genre": "Indie Rock / Pop Alternativo",
+    "location": "Santiago",
+    "description": "Amarga Marga es una banda de indie rock chilena conocida por su energía juvenil y su sonido fresco, mezclando ritmos bailables con guitarras limpias y letras emotivas. Destacaron con su disco debut 'Mi Año Nuclear'.",
+    "imageUrl": "https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/RGBS46M5K5CYZEQ4A2LLITKSHQ.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Niños del Cerro",
+    "genre": "Indie / Shoegaze / Folk Alternativo",
+    "location": "Santiago",
+    "description": "Niños del Cerro mezcla elementos de shoegaze, folk latinoamericano y texturas experimentales. Son reconocidos como uno de los proyectos más innovadores del indie chileno actual.",
+    "imageUrl": "https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/L5SNL2HXXBBC5J3BZXVRHJDU6M.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Prehistöricos",
+    "genre": "Indie Pop / Dream Pop",
+    "location": "Santiago",
+    "description": "Prehistöricos es una banda indie-pop chilena destacada por su sonido íntimo, emocional y atmosférico. Sus canciones combinan nostalgia, romanticismo y producción minimalista.",
+    "imageUrl": "https://soloartistaschilenos.cl/wp-content/uploads/2018/07/Prehistoricos.jpg"
+  },
+  {
+    "id":6,
+    "name": 'Patio Solar',
+    "genre": 'Indie Rock',
+    "location": 'Santiago',
+    "description": 'Patio Solar es una banda chilena de indie rock conocida por su sonido fresco y letras introspectivas. Formada en 2015, han lanzado varios álbumes que han sido bien recibidos por la crítica y el público.',
+    "imageUrl": 'https://www.musicapopular.cl/wp-content/uploads/2016/09/patiosolar-1.jpg'
+  }
+]
